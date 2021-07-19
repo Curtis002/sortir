8d4433d5e0dba1d17a636ff718ba1bdd90acee80
DESKTOP-7RBMLGI\Asus <anthony.romian2021@campus-eni.fr>
2021-07-19 16:28:07 +0200

    add check statuts cloturee des qu on click liste

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -14,11 +14,14 @@ use App\Form\CreateSortieType;
 use App\Repository\ParticipantRepository;
 use App\Form\SearchType;
 use App\Repository\SortieRepository;
+use App\Service\Statutchecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
+
 
 /**
  * @Route("/sorties", name="sortie_")
@@ -47,12 +50,38 @@ class SortieController extends AbstractController
     /**
      * @Route("/", name="list")
      */
-    public function list(SortieRepository $sortieRepository, Request $request)
+    public function list(SortieRepository $sortieRepository,EntityManagerInterface $entityManager, Request $request, Statutchecker $statutchecker)
     {
         $data = new SearchData();
         $sortieForm = $this->createForm(SearchType::class, $data);
         $sortieForm->handleRequest($request);
         $sorties = $sortieRepository->findSearch($data);
+
+        /////----test statutchecker-----//////// todo a integer dans le service statutchecker
+
+        for ($i = 0; $i <= count($sorties)-1; $i++){
+
+           $s = $sorties[$i];
+           $now = time();
+
+           $dateLimitCloture = $s->getDateLimiteInscription()->getTimestamp();
+
+           //var_dump($now);
+           //var_dump($dateLimitCloture);
+
+            if ($now > $dateLimitCloture && $s->getEtatSortie()->getid() != 3 )
+            {
+
+                $s->setEtatSortie($this->entityManager->getRepository(Etat::class)->findOneById(3));
+
+                $entityManager->refresh($s);
+                $entityManager->flush();
+
+            }
+
+        }
+        /////----test statutchecker-----////////
+
 
         return $this->render('sortie/list.html.twig', [
             'sorties'=>$sorties,
--- src/Service/Statutchecker.php
+++ src/Service/Statutchecker.php
@@ -5,30 +5,40 @@ namespace App\Service;
 
 use App\Entity\Etat;
 use App\Entity\Sortie;
+
+
 use Doctrine\ORM\EntityManagerInterface;
+
 
 class Statutchecker
 {
 
+    public function __construct( EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
 
 
-
-    public function statutSortie(EntityManagerInterface $entityManager)
+    public function statutClotureeSortie(Sortie $sortie)
     {
         // recup list de sorties
-        $sortierepo = $entityManager->getRepository(Sortie::class);
-        $etatrepo = $entityManager->getRepository(Etat::class);
-        $etat = $etatrepo->findAll();
-        var_dump($etat);
 
-        $sorties = $sortierepo->findAll();
-        var_dump($etat);
+
+
+
+
+
+        }
+
+
+        //$sortie->setEtatSortie(2);
+
 
 
         // comparé la date de cloture avec la date du jour now
 
         //si date depassé alors -> statut cloturee
 
-    }
+
 }
